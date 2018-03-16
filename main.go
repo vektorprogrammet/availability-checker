3fe913c1a369d472fa10c105d69452d7625f0f1b
Kristoffer <kra1992@hotmail.com>
2018-03-16 14:29:48 +0100

    Send slack message when site is back up

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 	"fmt"
 	"time"
 	"os"
+	"sync"
 )
 
 type Configuration struct {
 	SlackWorkspaces SlackWorkspaces `json:"slackWorkspaces"`
-	Websites []string `json:"websites"`
+	Websites        []string        `json:"websites"`
 }
 
 type SlackWorkspaces []Slack
@@ -33,9 +34,9 @@ type SlackMessage struct {
 
 func (s Slack) Send(message string) {
 	jsonData, _ := json.Marshal(SlackMessage{
-		Channel: s.Channel,
-		Text: message,
-		Username: s.Username,
+		Channel:   s.Channel,
+		Text:      message,
+		Username:  s.Username,
 		IconEmoji: s.IconEmoji,
 	})
 	http.Post(s.Endpoint, "application/json", bytes.NewBuffer(jsonData))
@@ -43,24 +44,38 @@ func (s Slack) Send(message string) {
 
 var slackWorkspaces SlackWorkspaces
 
-func checkWebsite(url string) {
+func checkWebsite(url string) bool {
 	resp, err := http.Get(url)
 	if err != nil {
-		sendWebsiteCrashAlert(url)
-		return
+		return false
 	}
 
-	fmt.Println(resp.StatusCode)
-
-	if resp.StatusCode != http.StatusOK {
-		sendWebsiteCrashAlert(url)
-		return
-	}
+	return resp.StatusCode == http.StatusOK
 }
 
 func sendWebsiteCrashAlert(url string) {
 	for _, slack := range slackWorkspaces {
-		slack.Send(fmt.Sprintf(":fire::fire::fire: %s er nede!!! :fire::fire::fire:", url))
+		slack.Send(fmt.Sprintf(":fire::fire::fire: %s is offline! :fire::fire::fire:", url))
+	}
+}
+
+func sendWebsiteBackUpAlert(url string) {
+	for _, slack := range slackWorkspaces {
+		slack.Send(fmt.Sprintf(":male-firefighter: %s is back online! :female-firefighter:", url))
+	}
+}
+
+func monitorWebsite(website string) {
+	isRunning := true
+	for {
+		wasRunning := isRunning
+		isRunning = checkWebsite(website)
+		if wasRunning && !isRunning {
+			sendWebsiteCrashAlert(website)
+		} else if !wasRunning && isRunning {
+			sendWebsiteBackUpAlert(website)
+		}
+		time.Sleep(1 * time.Minute)
 	}
 }
 
@@ -86,10 +101,12 @@ func main() {
 	slackWorkspaces = configuration.SlackWorkspaces
 	websites := configuration.Websites
 
-	for {
-		for _, website := range websites {
-			checkWebsite(website)
-		}
-		time.Sleep(1 * time.Minute)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	for _, website := range websites {
+		go monitorWebsite(website)
 	}
+
+	wg.Wait()
 }
